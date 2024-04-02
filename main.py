cef9e045ee36a27ca833aa60b5681d1cb6749f89
Serhii Hrekov <themebeat@gmail.com>
2024-04-03 02:13:44 +0300

    poetry add uvicorn run with main.py and uvicorn, port 8000 via main:app

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from telegram_to_wordpress.telegram_bot.core import run_bot
+import uvicorn
+
 
 def app():
     print('Starting bot...')
     run_bot()
 
 if __name__ == '__main__':
-    #run with uvicorn main:app --reload
-    import uvicorn
 
-    uvicorn.run("main:app", host="8000", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0",port=8000, reload=True)
+
 
